0c10f46c8c35e188285fb1306554ed86b13bebf3
Armando Fox <fox@cs.berkeley.edu>
2012-01-04 08:47:51 -0800

    instance-eval will allow defining question types as instance methods on quiz

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -1,7 +1,7 @@
 require 'builder'
 
 class Quiz
-  
+
   def default_options
     {
       :open_time => Time.now,
@@ -9,38 +9,44 @@ class Quiz
       :hard_close_time => Time.now + 24*60*60,
       :maximum_submissions => 1,
       :duration => 0,
+      :retry_delay => 600,
+      :version => 'First version',
+      :default_question_group_break => 'continue-previous-section',
+      :randomise_question_group_order => false,
+      :randomise_question_order => false,
+      :randomise_option_order => false,
+      :feedback_immediate => 'all_explanations',
+      :feedback_after_hard_deadline => 'all_explanations',
     }
   end
 
   def initialize(title, options)
     @output = ''
     @xml = Builder::XmlMarkup.new(:target => @output)
-    @xml.instruct!
-    @xml.declare! :DOCTYPE, :quiz, :SYSTEM, "quiz.dtd"
     @title = title
     @options = default_options.merge(options)
-    @xml.quiz do
-      emit_metadata
-    end
+    self.do_prologue
   end
 
   def render ; @output ; end
   
-  def self.quiz(title, options={})
+  def self.quiz(title, options={}, &block)
     quiz = Quiz.new(title, options)
-    debugger
+    quiz.instance_eval(&block) if block_given?
+    quiz.render
   end
 
-  private
-
-  def emit_metadata
-    @xml.metadata do
-      @xml.type 'quiz'
-      @xml.title @title
-      @options.each_pair do |k,v|
-        @xml.__send__(k.to_s.gsub(/_/,'-').to_sym, v)
+  def do_prologue
+    @xml.instruct!
+    @xml.declare! :DOCTYPE, :quiz, :SYSTEM, "quiz.dtd"
+    @xml.quiz do
+      @xml.metadata do
+        @xml.type 'quiz'
+        @xml.title @title
+        @options.each_pair do |k,v|
+          @xml.__send__(k.to_s.gsub(/_/,'-').to_sym, v)
+        end
       end
     end
   end
-
 end
--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -3,9 +3,21 @@ require 'quiz'
 require 'ruby-debug'
 
 describe Quiz do
-  describe 'new quiz' do
+  describe 'should include XML elements' do
     subject { Quiz.new('Foo', :maximum_submissions => 2, :start => '2011-01-01 00:00', :time_limit => 60).render }
-    it { should have_xml_element 'quiz/metadata/title', :value => 'Foo' }
-    it { should have_xml_element 'quiz/metadata/maximum-submissions', :value => 2 }
+    {'title' => 'Foo',
+      'maximum-submissions' => '2',
+      'type' => 'quiz' }.each_pair do |element, value|
+      it { should have_xml_element "quiz/metadata/#{element}", :value => value }
+    end
+    %w(retry-delay open-time soft-close-time hard-close-time duration retry-delay maximum-submissions feedback-immediate feedback-after-hard-deadline randomise-question-group-order randomise-question-order randomise-option-order version default-question-group-break).each do |element|
+      it { should have_xml_element "quiz/metadata/#{element}" }
+    end
   end
+  describe 'parsing questions' do
+    it 'should not raise error' do
+      lambda { Quiz.quiz('foo') {  } }.should_not raise_error
+    end
+  end
+        
 end
