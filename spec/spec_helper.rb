5211d0bd1f2efaba79d2302d0073616e93d33514
Jesmin Ngo <xjesmin.ngo@gmail.com>
2016-04-15 22:22:42 +0000

    updated from deprecated dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,3 @@
-require 'ruby-debug'
+require 'byebug'
 Dir[File.join(File.dirname(__FILE__), '..', 'lib', '*.rb')].each { |f|  load f }
 Dir[File.join(File.dirname(__FILE__), '/support', '**', '*.rb')].each {|f| load f}
