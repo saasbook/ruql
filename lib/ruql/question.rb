25771ab4a30022ccc6aa0f677a4424a9b9762f12
Steven Wu <stevenwuyinze@berkeley.edu>
2016-11-09 08:14:32 +0000

    Tried to resolve no global_explanation Exception

--- lib/ruql/question.rb
+++ lib/ruql/question.rb
@@ -92,7 +92,7 @@ class Question
     hash = JSON.parse(hash_str)
     #create the appropriate class of the object from the hash's class name
     question = Object.const_get(hash.fetch('question_type')).new()
-    hash.reject{|key| key == 'answers' or key == 'question_type'}.each do |key, value|
+    hash.reject{|key| key == 'answers' or key == 'question_type' or key == 'global_explanation'}.each do |key, value|
       begin
         question.send((key + '=').to_sym, value)
       rescue
