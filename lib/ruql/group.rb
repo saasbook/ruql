7f45ad29ab379e58c8c470e7a83d020bea46217d
Steven Wu <stevenwuyinze@berkeley.edu>
2016-11-16 23:36:23 +0000

    Experiment with grouped question

---
+++ lib/ruql/group.rb
@@ -0,0 +1,40 @@
+class Group < Question
+    attr_accessor :questions
+    def initialize(*args)
+        log = Logger.new(STDERR)
+        log.debug "in select_multiple"
+    end
+    
+    def choice_answer(*args, &block)
+        if args.first.is_a?(Hash) # no question text
+          q = MultipleChoice.new('',*args)
+        else
+          text = args.shift
+          q = MultipleChoice.new(text, *args)
+        end
+        q.instance_eval(&block)
+        @questions << q
+    end
+  
+    def select_multiple(*args, &block)
+        if args.first.is_a?(Hash) # no question text
+            q = SelectMultiple.new('', *args)
+        else
+            text = args.shift
+            q = SelectMultiple.new(text, *args)
+        end
+            q.instance_eval(&block)
+        @questions << q
+    end
+
+    def truefalse(*args)
+        q = TrueFalse.new(*args)
+        @questions << q
+    end
+
+    def dropdown(*args, &block)
+        q = Dropdown.new(*args)
+        q.instance_eval(&block)
+        @questions << q
+    end
+end
