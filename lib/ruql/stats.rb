cad7e55b187baaff3368ed93109a44d33cbf942e
Armando Fox <fox@cs.berkeley.edu>
2020-06-03 12:08:47 -0700

    fix bugs in command line parsing

--- lib/ruql/stats.rb
+++ lib/ruql/stats.rb
@@ -8,10 +8,10 @@ module Ruql
     end
     def render_quiz
       @output << "%3d questions" % quiz.questions.length
-      @output << "%3d questions (%d points) in no group" % [quiz.ungrouped_questions.length, quiz.ungrouped_points]
-      @output << "%3d questions (%d points) in %d groups" % [quiz.grouped_questions.length, quiz.grouped_points, quiz.groups.length]
+      @output << "    %3d (%d points) in no group" % [quiz.ungrouped_questions.length, quiz.ungrouped_points]
+      @output << "    %3d (%d points) in %d groups" % [quiz.grouped_questions.length, quiz.grouped_points, quiz.groups.length]
       @output << "%3d effective questions on quiz" % quiz.num_questions
-      @output << "%3d effective points" % quiz.points
+      @output << "%3d max points possible" % quiz.points
       @output.join("\n")
     end
   end
